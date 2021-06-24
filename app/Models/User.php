5810631aa8c00452cd5da3153de77dee19a3acd8
s1107777 <s1107777@student.hsleiden.nl>
2021-06-24 17:13:27 +0200

    change incrementing

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens;
 
-    public $incrementing = true; 
+    public $incrementing = false; 
 
     /**
      * The attributes that are mass assignable.
