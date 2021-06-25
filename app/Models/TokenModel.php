84ab0f0757bfcef0657a6c4b205782213db0f968
s1107777 <s1107777@student.hsleiden.nl>
2021-06-25 10:16:45 +0200

    add tokenModel

---
+++ app/Models/TokenModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Sanctum\PersonalAccessToken;
+
+class TokenModel extends PersonalAccessToken
+{
+    protected $table;
+
+    public function __construct() {
+
+        $this->table = config('app.db_table_prefix') . 'tokens';
+
+    }
+}
