d302877ae169ce02380d57b764a19463aade828d
s1107777 <s1107777@student.hsleiden.nl>
2021-06-25 10:57:35 +0200

    change AuthController

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Closure;
 
 class Authenticate extends Middleware
 {
@@ -20,13 +19,4 @@ class Authenticate extends Middleware
         }
     }
 
-    public function handle($request, Closure $next, ...$guards)
-    {
-        if($jwt = $request->cookie('jwt')){
-            $request->headers->set('Authorization', 'Bearer ' . $jwt);
-        }
-        $this->authenticate($request, $guards);
-
-        return $next($request);
-    }
 }
