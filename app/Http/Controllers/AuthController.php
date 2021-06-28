4b8319fc933ab434e7552ba8efb298d004c16888
s1107777 <s1107777@student.hsleiden.nl>
2021-06-28 11:44:14 +0200

    add vendor map

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
