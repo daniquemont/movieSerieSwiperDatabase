e578141cef7095c09dd381e8c176b0f684a6ef4b
s1107777 <s1107777@student.hsleiden.nl>
2021-06-28 16:46:38 +0200

    change register code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,17 +13,17 @@ class AuthController extends Controller
 {
     public function register(Request $request){
 
-        $fields = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|string|email',
-            'password' => 'required|string',
+        // $fields = $request->validate([
+        //     'name' => 'required|string',
+        //     'email' => 'required|string|email',
+        //     'password' => 'required|string',
             
-        ]);
+        // ]);
 
         $user = User::create([
-            'name' => $fields['name'],
-            'email' => $fields['email'],
-            'password' => bcrypt($fields['password'])
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
         ]);
 
         // $token = $user->createToken($request->name)->plainTextToken;
