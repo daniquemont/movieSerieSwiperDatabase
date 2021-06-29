269e3b9bb5ee6462ec9518882fc2d7dbfee09169
s1107777 <s1107777@student.hsleiden.nl>
2021-06-29 11:05:20 +0200

    add dd log

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -55,11 +55,11 @@ class AuthController extends Controller
             ]);
         }
         dd(
-            User::find(Auth::id('user_id')) ?? '?',
+            // User::find(Auth::id('user_id')) ?? '?',
             // User::find(Auth::id()) ?? '?',
             User::find(Auth::user()) ?? '?',
             // User::find(Auth::user()->id()) ?? '?',
-            User::find(Auth::user()->id('user_id')) ?? '?',
+            // User::find(Auth::user()->id('user_id')) ?? '?',
             // $token ?? '?'
         );
         
